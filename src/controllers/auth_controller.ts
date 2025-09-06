96f8875b5edf634737bb6074187525cea7e13b9c
Christentimmy <timmychris09@gmail.com>
2025-09-06 21:02:20 +0300

    auth controller

--- src/controllers/auth_controller.ts
+++ src/controllers/auth_controller.ts
@@ -5,6 +5,7 @@ import bcryptjs from "bcryptjs";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import { redisController } from "./redis_controller";
 
 const isValidObjectId = mongoose.Types.ObjectId.isValid;
 dotenv.config();
@@ -120,6 +121,84 @@ export const authController = {
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: "Internal server error" });
+    }
+  },
+
+  sendNumberOtp: async (req: Request, res: Response) => {
+    try {
+      if (!req.body || typeof req.body !== "object") {
+        res.status(400).json({ message: "Missing request body" });
+        return;
+      }
+
+      const { phone } = req.body;
+      if (!phone) {
+        res.status(400).json({ message: "Phone number is required" });
+        return;
+      }
+
+      // const response = await checkNumberValidity(phone);
+      // if (!response) {
+      //   res.status(400).json({ message: "Number is not valid" });
+      //   return;
+      // }
+
+      // const { success, otp, error } = await sendOtpToUserPhoneNumber(phone);
+
+      // if (!success || !otp) {
+      //   res.status(500).json({ message: error });
+      //   return;
+      // }
+
+      // await redisController.saveOtpToStore(phone, otp.toString());
+
+      res.status(200).json({ message: "OTP sent successfully." });
+      return;
+    } catch (error) {
+      console.error("Error in requestOTP controller:", error);
+      res
+        .status(500)
+        .json({ message: "An error occurred while processing your request." });
+    }
+  },
+
+  verifyNumberOtp: async (req: Request, res: Response) => {
+    try {
+      if (!req.body || typeof req.body !== "object") {
+        res.status(400).json({ message: "Missing request body" });
+        return;
+      }
+      const { phone, otp } = req.body;
+      if (!phone || !otp) {
+        res.status(400).json({ message: "Phone number and OTP are required" });
+        return;
+      }
+      // const response = await checkNumberValidity(phone);
+      // if (!response) {
+      //   res.status(400).json({ message: "Number is not valid" });
+      //   return;
+      // }
+
+      // const storedOtp = await redisController.getOtpFromStore(phone);
+      // if (!storedOtp) {
+      //   res.status(400).json({ message: "OTP not found" });
+      //   return;
+      // }
+
+      // if (storedOtp !== otp) {
+      //   res.status(400).json({ message: "Invalid OTP" });
+      //   return;
+      // }
+
+      // await redisController.removeOtp(phone);
+
+      // res.status(200).json({ message: "OTP verified successfully." });
+      return;
+    } catch (error) {
+      console.error("Error in requestOTP controller:", error);
+      res
+        .status(500)
+        .json({ message: "An error occurred while processing your request." });
     }
   },
 };
