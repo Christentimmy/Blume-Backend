086e36a88b9017478dfd946ff272950fe33136d5
Christentimmy <timmychris09@gmail.com>
2026-01-15 02:04:14 +0300

    subscription plan

--- src/config/subscription_plans.ts
+++ src/config/subscription_plans.ts
@@ -1,75 +1,95 @@
-
 import dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.FALANGBASICPRICEID || !process.env.FALANGPREMIUMPRICEID || !process.env.FALANGPREMIUMPLUSPRICEID) {
+if (
+  !process.env.BLUME1WEEKPRICEID ||
+  !process.env.BLUME1MONTHPRICEID ||
+  !process.env.BLUME3MONTHPRICEID ||
+  !process.env.BLUME1YEARPRICEID
+) {
   throw new Error("Please add the subscription plans to the .env file");
 }
 
 export const PLANS = {
-  BASIC: {
-    id: "basic_monthly",
-    name: "Basic",
-    billingCycle: "month",
-    billingPeriodMonths: 1,
-    price: 9,
+  WEEK1: {
+    id: "1week",
+    name: "1 week",
+    billingCycle: "$14.99/week",
+    // billingPeriodMonths: 1,
+    price: 14.99,
     currency: "USD",
-    stripePriceId: process.env.FALANGBASICPRICEID.toString(),
+    stripePriceId: process.env.BLUME1WEEKPRICEID.toString(),
     features: [
-      "Unlimited swipes",
-      "See who viewed your profile",
+      "Unlimited Swipes",
+      "Unlimited Likes",
       "Rewind last swipe",
+      "First look at new members",
+      "See who viewed your profile",
       "Super Likes per day: 1",
-      "Boosts per month: 1",
     ],
     limits: {
       superLikesPerDay: 1,
-      boostsPerMonth: 1,
     },
   },
-  PREMIUM: {
-    id: "premium_6_months",
-    name: "Premium",
-    billingCycle: "6_months",
-    billingPeriodMonths: 6,
-    price: 54,
+  MONTH1: {
+    id: "1month",
+    name: "1 month",
+    billingCycle: "$4.99/week",
+    // billingPeriodMonths: 6,
+    price: 19.99,
     currency: "USD",
-    stripePriceId: process.env.FALANGPREMIUMPRICEID.toString(), 
+    stripePriceId: process.env.BLUME1MONTHPRICEID.toString(),
     features: [
-      "All Basic features",
-      "Advanced filters",
-      "See who liked you",
-      "Ad-free experience",
-      "Super Likes per day: 3",
-      "Boosts per month: 2",
+      "Unlimited Swipes",
+      "Unlimited Likes",
+      "Rewind last swipe",
+      "First look at new members",
+      "See who viewed your profile",
+      "Super Likes per day: 1",
     ],
     limits: {
       superLikesPerDay: 3,
-      boostsPerMonth: 2,
     },
   },
-  PREMIUM_PLUS: {
-    id: "premium_plus_12_months",
-    name: "Premium Plus",
-    billingCycle: "12_months",
-    billingPeriodMonths: 12,
-    price: 108,
+  MONTH3: {
+    id: "3months",
+    name: "3 months",
+    billingCycle: "$3.33/week",
+    // billingPeriodMonths: 12,
+    price: 39.99,
     currency: "USD",
-    stripePriceId: process.env.FALANGPREMIUMPLUSPRICEID.toString(), 
+    stripePriceId: process.env.BLUME3MONTHPRICEID.toString(),
     features: [
-      "All Premium features",
-      "Change location (Travel mode)",
-      "Priority support",
-      "Super Likes per day: 5",
-      "Boosts per month: 3",
+      "Unlimited Swipes",
+      "Unlimited Likes",
+      "Rewind last swipe",
+      "First look at new members",
+      "See who viewed your profile",
+      "Super Likes per day: 1",
     ],
     limits: {
       superLikesPerDay: 5,
       boostsPerMonth: 3,
     },
   },
+  YEAR1: {
+    id: "1year",
+    name: "1 year",
+    billingCycle: "$1.53/week",
+    // billingPeriodMonths: 12,
+    price: 79.99,
+    currency: "USD",
+    stripePriceId: process.env.BLUME1YEARPRICEID.toString(),
+    features: [
+      "Unlimited Swipes",
+      "Unlimited Likes",
+      "Rewind last swipe",
+      "First look at new members",
+      "See who viewed your profile",
+      "Super Likes per day: 1",
+    ],
+    limits: {
+      superLikesPerDay: 5,
+    },
+  },
 } as const;
-
-
-
-
